bb5d0816eb8af44c30d62c8229967c3c08fd124e
Nikola Rozic <nikola@Nikolas-MacBook-Pro.local>
2017-01-21 16:11:50 +0100

    Code refactoring, removing unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,164 +1,84 @@
 import { Component } from '@angular/core';
 import { DataService } from './app.service';
 import 'rxjs/add/operator/toPromise';
-
-
 @Component({
-	selector: 'converter',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
-	providers: [ DataService ],
+    selector: 'converter',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [ DataService ],
 })
 
 export class AppComponent {
+    constructor(private dataService: DataService) {}
+    title: string = 'Angular 2 Currency Converter made by future Bulb employee';
+    error: any = null;
+    fromAmount: number = 10;
+    toAmount: number = 0;
+    fromCurrency: string = null;
+    toCurrency: string = null;
+    rates: Array<any> = [];
+    fromRates = {};
 
-	constructor(private dataService: DataService) {}
-	
-	title: string = 'Angular 2 Currency Converter made by future Bulb employee';
-	error: any = null;
-	fromAmount: number = 0;
-	toAmount: number = 0;
-	fromCurrency: string = null;
-	toCurrency: string = null;
-	rates: Array<any> = [];
-	fromRates = {};
+    ngOnInit() {
+        this.convert(false, true);
+    }
 
-	ngOnInit() {
-		// Get data from API when App is initialized
-		this.getData();
-	}
+    private convert(reverse, initial) {
+        this.error = null;
 
-	getData() {
-		this.dataService.getRates('').then(response => {
-			console.log('Initial rates:', response);
+        this.dataService.getRates(this.fromCurrency).then(response => {
 
-			var items: Array<any> = [];
-			
-			// In case we got a response from API, we will parse result to get list of rates
-			if(response.rates) {
-				for (var key in response.rates){
-					var obj = {
-						id: key,
-						value: response.rates[key]
-					};
-					// Store data to items variable
-					items.push(obj);
-				}
-				// Dirty hack to add base rate to rates list :)
-				items.push({id: 'EUR', value: 1});
-				// Time to assign data to variable
-				this.rates = items;
-				// Set initial currencies
-				this.fromCurrency = this.rates[29].id;
-				this.toCurrency = this.rates[10].id;
-				// And not to forget to set initial value as well
-				this.fromAmount = 100;
-				this.toAmount = 0;
-				this.recalculate();
-				console.log(this.rates);
-			} else {
-				// If there is no response, something is went wrong
-				this.error = 'Unable to get data data from API';
-			}
-		}, () => {
-			this.error = 'App was unable to request rates from fixer.io API. \n If error presists, hire me to fix it for you :)';
-		});
-	}
+            if (response.rates) {
 
-	private recalculate() {
-		this.onFromChange();
-	}
+                if(initial) {
+                    let items: Array<any> = [];
 
-	private onFromChange() {
-		// Making sure that function is working :)
-		console.log('On from change');
-		this.error = null;
-		// Is still everything OK?
-		console.log('Selected from: ', this, this.fromCurrency);
+                    for (let key in response.rates){
+                        if (key) {
+                            let obj = {
+                                id: key,
+                                value: response.rates[key]
+                            };
+                            items.push(obj);
+                        }
+                    }
 
-		this.dataService.getRates(this.fromCurrency).then(response => {
-			// keep an eye on console to make sure that we are actually recieving data
-			console.log('OnFromChange response: ', response);
+                    items.push({id: 'EUR', value: 1});
+                    this.rates = items;
+    				this.fromCurrency = this.rates[29].id;
+    				this.toCurrency = this.rates[10].id;
+                    this.convert(false, false);
+                }
 
-			if (response.rates) {
-				this.fromRates = response.rates;
-				this.onFromAmountChange();
-			} else {
-				this.error = 'Unable to get data data from API';
-			}
-		}, () => {
-			this.error = 'Unable to request rates';
-		});
-	}
-	
-	private onToChange() {
-		// Log to console so we can see what is happening
-		console.log("onToChange", this, this.toCurrency);
-		this.error = null;
-		this.onFromAmountChange();
-	}
+                this.fromRates = response.rates;
 
-	private onFromAmountChange() {
-		console.log('onFromAmountChange', this, this.fromAmount);
-		this.error = null;
-		
-		// Make sure that amount for conversion is set
-		if (this.fromAmount === 0 || !this.fromAmount) {
-			this.toAmount = 0;
-			this.error = 'We can\'t convert nothing into nothing :D';
-			return;
-		}
-		
-		// Make sure that currency is set, although we have set them initially, but you never know :D
-		if (!this.toCurrency) {
-			this.toAmount = 0;
-			this.error = 'Please set currency';
-			return;
-		}
-		
-		// what if someone decide to convert USD to USD? :D
-		if (this.toCurrency === this.fromCurrency) {
-			this.toAmount = this.fromAmount;
-			this.error = 'Converting ' + this.toCurrency + ' to ' + this.fromCurrency + ' doesn\'t make much sense, does it?';
-			return;
-		}
-	
-		var number = this.fromAmount * this.fromRates[this.toCurrency];
-		//$log.debug(number);
-		number = Math.round(number * 100);
-		//$log.debug(number);
-		this.toAmount = number / 100;
-		//$log.debug(vm.data.toNumber);
-	}
+                if (this.fromAmount === 0 || !this.fromAmount ) {
+                    this.error = 'We can\'t convert nothing into nothing :D';
+                    return;
+                }
 
-	private onToAmountChange() {
-		console.log('onToAmountChange', this, this.toAmount);
-		this.error = null;
-		
-		// Make sure that amount for conversion is set
-		if (this.toAmount === 0 || !this.toAmount) {
-			this.fromAmount = 0;
-			this.error = 'We can\'t convert nothing into nothing :D';
-			return;
-		}
-		
-		// Make sure that currency is set, although we have set them initially, but you never know :D
-		if (!this.fromCurrency) {
-			this.fromAmount = 0;
-			this.error = 'Please set currency';
-			return;
-		}
-		
-		// what if someone decide to convert USD to USD? :D
-		if (this.toCurrency === this.fromCurrency) {
-			this.fromAmount = this.toAmount;
-			this.error = 'Converting ' + this.toCurrency + ' to ' + this.fromCurrency + ' doesn\'t make much sense, does it?';
-			return;
-		}
-		
-		// Let's do the math and make sure that number is rounded to 2 decimal points
-		var number = this.toAmount / this.fromRates[this.toCurrency];
-		number = Math.round(number * 100);
-		this.fromAmount = number / 100;
-	}
+                if (!this.fromCurrency) {
+                    this.error = 'Please set currency';
+                    return;
+                }
+
+                if (this.toCurrency === this.fromCurrency) {
+                    this.fromAmount = this.toAmount;
+                    this.error = 'Converting ' + this.toCurrency + ' to ' + this.fromCurrency + ' doesn\'t make much sense, does it?';
+                    return;
+                }
+
+                if (reverse) {
+                    this.fromAmount = Math.round( this.toAmount / this.fromRates[this.toCurrency] * 100) / 100;
+                } else {
+                    this.toAmount = Math.round(this.fromAmount * this.fromRates[this.toCurrency] * 100) / 100;
+                }
+
+            } else {
+                this.error = 'Unable to get data from API';
+            }
+        }, (error)=> {
+            this.error = 'Došlo je do greške: ' + error.status + ' - ' + error.statusText;
+        });
+    }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,32 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class DataService {
-	private apiUrl = 'http://api.fixer.io/latest';
-	constructor(private http: Http) {}
+    private apiUrl = 'http://api.fixer.io/latest1';
+    constructor(private http: Http) {}
 
-	getRates(base): Promise<any> {
-		// Check if base is set and customize API url accordingly
-		if(base){
-			var url = this.apiUrl + '?base=' + base;
-		} else {
-			var url = this.apiUrl;
-		}
+    getRates(base): Promise<any> {
+        let url: string;
 
-		// We could expand this even further by adding date picker to select historycal conversion and if statement that will check if date is selected, but for now it will just check for latest rates available
-		
-		// Get rates from API
-		return this.http.get(url)
-			.toPromise()
-			.then(rsp => rsp.json())
-			.catch(this.errorHandler);
-	}
+        // Check if base is set and customize API url accordingly
+        if(base){
+            url = this.apiUrl + '?base=' + base;
+        } else {
+            url = this.apiUrl;
+        }
 
-	private errorHandler(error: any): Promise<any> {
-		// log error to console
-		console.error('There was an error', error);
-		return Promise.reject(error.message || error);
-	}
+        // Get rates from API
+        return this.http.get(url)
+            .toPromise()
+            .then(rsp => rsp.json())
+            .catch(this.errorHandler);
+    }
+
+    private errorHandler(error: any): Promise<any> {
+        // log error to console
+        console.error('There was an error', error);
+        return Promise.reject(error.message || error);
+    }
 }
